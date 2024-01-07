f98736bac165f52fc2b8eee0d80b7d306de8f5fa
Diegodrou <diegodroulers@gmail.com>
2024-01-07 16:03:22 +0100

    refactorisation

--- src/main/actions/carte/Carte.java
+++ src/main/actions/carte/Carte.java
@@ -6,16 +6,6 @@ import main.actions.ActionEntite;
 public abstract class Carte extends ActionEntite {
     private int cout;
 
-    /**
-     * Indique si les effets de la carte s'applique a un monstre ou a un heros
-     * 
-     * @return un String "Heros" si applicable sur un objet de type Heros, "Monstre"
-     *         si applicable au monstre
-     */
-    public String getTypeDentiteApplicable() {
-        return typeDentiteApplicable;
-    }
-
     public Carte(String typeDentiteApplicable, int cout, int nb_degats, int nb_block) {
         super(typeDentiteApplicable, nb_degats, nb_block);
         this.cout = cout;
